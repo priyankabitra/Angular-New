c36f65bd36e51ac912745981fee3742ed4a791e1
priyankabitra <bitrapriyanka238@gmail.com>
2022-08-11 16:24:13 +0530

    login form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
 import { StudentresultsComponent } from './studentresults/studentresults.component';
 import { Table1Component } from './table1/table1.component';
 
 const routes: Routes = [
   {path:"studentresults",component:StudentresultsComponent},
   {path:"table1",component:Table1Component},
+  {path:"login",component:LoginComponent},
+  {path:"registration",component:RegistrationComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Table1Component } from './table1/table1.component';
 import { StudentresultsComponent } from './studentresults/studentresults.component';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     Table1Component,
-    StudentresultsComponent
+    StudentresultsComponent,
+    LoginComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
